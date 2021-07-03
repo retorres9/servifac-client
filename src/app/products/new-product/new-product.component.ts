f84d47a164808e6e722e0835810ef28ed5883d10
retorres <roberthtorres40@yahoo.es>
2021-07-02 22:26:49 -0500

    Added gain inputs in new product form

--- src/app/products/new-product/new-product.component.ts
+++ src/app/products/new-product/new-product.component.ts
@@ -17,29 +17,47 @@ export class NewProductComponent implements OnInit {
         updateOn: 'change',
         validators: [Validators.required]
       }),
+      prod_code: new FormControl("", {
+        updateOn: 'change',
+        validators: [Validators.required, Validators.maxLength(13)]
+      }),
       prod_price: new FormControl("", {
         updateOn: 'change',
         validators: [Validators.required, Validators.min(0)]
       }),
-      prod_priceMajor: new FormControl("", {
+      prod_retailGain: new FormControl(20, {
+        updateOn: 'change',
+        validators: [Validators.required]
+      }),
+      prod_wholesaleGain: new FormControl(15 , {
+        updateOn: 'change',
+        validators: [Validators.required]
+      }),
+      prod_retailPrice: new FormControl("", {
+        updateOn: 'change',
+        validators: [Validators.required, Validators.min(0.05)]
+      }),
+      prod_wholesalePrice: new FormControl("", {
         updateOn: 'change',
         validators: [Validators.required, Validators.min(0)]
       }),
       quantity: new FormControl("", {
         updateOn: 'change',
-        validators: [Validators.required]
+        validators: [Validators.required, Validators.min(0)]
       }),
       minQuantity: new FormControl("", {
         updateOn: 'change',
-        validators: [Validators.required]
+        validators: [Validators.required, Validators.min(0)]
       })
     })
   }
 
   calculatePrice() {
     console.log(Number((this.newProductForm.value.prod_price * 0.32) + this.newProductForm.value.prod_price).toFixed(2));
-    console.log(Number((this.newProductForm.value.prod_price * 0.22) + this.newProductForm.value.prod_price).toFixed(2));
-    this.newProductForm.controls['prod_priceMajor'].setValue(Number((this.newProductForm.value.prod_price * 0.22) + this.newProductForm.value.prod_price).toFixed(2));
+    console.log(Number((this.newProductForm.value.prod_price * 0.27) + this.newProductForm.value.prod_price).toFixed(2));
+    this.newProductForm.controls['prod_wholesalePrice'].setValue(Number((this.newProductForm.value.prod_price * 0.27) + this.newProductForm.value.prod_price).toFixed(2));
+    this.newProductForm.controls['prod_retailPrice'].setValue(Number((this.newProductForm.value.prod_price * 0.32) + this.newProductForm.value.prod_price).toFixed(2));
+
   }
 
 }
