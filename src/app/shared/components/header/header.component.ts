5ffbde810fb515b293c3aa7fdd70435a32dbc745
retorres <roberthtorres40@yahoo.es>
2021-11-13 11:10:29 -0500

    Redesigned notifications in header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { CredentialsJwt } from '../../../auth/jwt-credentials.model';
 import jwt_decode from 'jwt-decode';
@@ -12,8 +12,11 @@ import { ProductsService } from '../../../products/products.service';
 export class HeaderComponent implements OnInit {
   alert: boolean;
   user: string;
+
+  isChange: boolean = false;
+
   @Input() tab: string;
-  constructor(private router: Router, private productService: ProductsService) { }
+  constructor(private router: Router, private productService: ProductsService, private eRef: ElementRef) { }
 
   ngOnInit(): void {
     const token = localStorage.getItem('token');
@@ -26,7 +29,23 @@ export class HeaderComponent implements OnInit {
 
   }
 
+  @HostListener('document:click',['$event'])
+  closeNotifications(event) {
+    if (this.eRef.nativeElement.contains(event.target)) {
+      console.log('in');
+    } else {
+      this.isChange = false;
+
+    }
+  }
+  toggleNotifications() {
+    this.isChange = !this.isChange;
+    console.log(this.isChange);
+
+  }
+
   goToMinimums() {
+
     this.router.navigateByUrl('products/minimums');
   }
 
